a2935ef3a055efc6d6216e4fba7f44da2af913fe
NateWeiler <nateweiler84@gmail.com>
2020-06-18 22:33:12 -0400

    Add

---
+++ vscode-pull-request-github/src/authentication/githubServer.ts
@@ -0,0 +1,226 @@
+import * as https from 'https';
+import * as vscode from 'vscode';
+import Logger from '../common/logger';
+import { agent } from '../common/net';
+import { handler as uriHandler } from '../common/uri';
+import { PromiseAdapter, promiseFromEvent } from '../common/utils';
+import { HostHelper, IHostConfiguration } from './configuration';
+import { listHosts, onDidChange as onKeychainDidChange, toCanonical } from './keychain';
+import uuid = require('uuid');
+
+const SCOPES: string = 'read:user user:email repo write:discussion';
+const GHE_OPTIONAL_SCOPES: { [key: string]: boolean } = {'write:discussion': true};
+
+const AUTH_RELAY_SERVER = 'vscode-auth.github.com';
+
+export class GitHubManager {
+	private _servers: Map<string, boolean> = new Map().set('github.com', true);
+
+	private static GitHubScopesTable: { [key: string]: string[] } = {
+		repo: ['repo:status', 'repo_deployment', 'public_repo', 'repo:invite'],
+		'admin:org': ['write:org', 'read:org'],
+		'admin:public_key': ['write:public_key', 'read:public_key'],
+		'admin:org_hook': [],
+		gist: [],
+		notifications: [],
+		user: ['read:user', 'user:email', 'user:follow'],
+		delete_repo: [],
+		'write:discussion': ['read:discussion'],
+		'admin:gpg_key': ['write:gpg_key', 'read:gpg_key']
+	};
+
+	public static AppScopes: string[] = SCOPES.split(' ');
+
+	public async isGitHub(host: vscode.Uri): Promise<boolean> {
+		if (host === null) {
+			return false;
+		}
+
+		if (this._servers.has(host.authority)) {
+			return !!this._servers.get(host.authority);
+		}
+
+		const keychainHosts = await listHosts();
+		if (keychainHosts.indexOf(toCanonical(host.authority)) !== -1) {
+			return true;
+		}
+
+		const options = GitHubManager.getOptions(host, 'HEAD', '/rate_limit');
+		return new Promise<boolean>((resolve, _) => {
+			const get = https.request(options, res => {
+				const ret = res.headers['x-github-request-id'];
+				resolve(ret !== undefined);
+			});
+
+			get.end();
+			get.on('error', err => {
+				Logger.appendLine(`No response from host ${host}: ${err.message}`, 'GitHubServer');
+				resolve(false);
+			});
+		}).then(isGitHub => {
+			Logger.debug(`Host ${host} is associated with GitHub: ${isGitHub}`, 'GitHubServer');
+			this._servers.set(host.authority, isGitHub);
+			return isGitHub;
+		});
+	}
+
+	public static getOptions(hostUri: vscode.Uri, method: string = 'GET', path: string, token?: string) {
+		const headers: {
+			'user-agent': string;
+			authorization?: string;
+		} = {
+			'user-agent': 'GitHub VSCode Pull Requests',
+		};
+		if (token) {
+			headers.authorization = `token ${token}`;
+		}
+
+		return {
+			host: HostHelper.getApiHost(hostUri).authority,
+			port: 443,
+			method,
+			path: HostHelper.getApiPath(hostUri, path),
+			headers,
+			agent,
+		};
+	}
+
+	public static validateScopes(host: vscode.Uri, scopes: string): boolean {
+		if (!scopes) {
+			Logger.appendLine(`[SKIP] validateScopes(${host.toString()}): No scopes available.`);
+			return true;
+		}
+		const tokenScopes = scopes.split(', ');
+		const scopesNotFound = this.AppScopes.filter(x => !(
+			tokenScopes.indexOf(x) >= 0 ||
+			tokenScopes.indexOf(this.getScopeSuperset(x)) >= 0 ||
+			// some scopes don't exist on older versions of GHE, treat them as optional
+			(this.isDotCom(host) || GHE_OPTIONAL_SCOPES[x])
+		));
+		if (scopesNotFound.length) {
+			Logger.appendLine(`[FAIL] validateScopes(${host.toString()}): ${scopesNotFound.length} scopes missing`);
+			scopesNotFound.forEach(scope => Logger.appendLine(`   - ${scope}`));
+			return false;
+		}
+		return true;
+	}
+
+	private static getScopeSuperset(scope: string): string {
+		for (const key in this.GitHubScopesTable) {
+			if (this.GitHubScopesTable[key].indexOf(scope) >= 0) {
+				return key;
+			}
+		}
+		return scope;
+	}
+
+	private static isDotCom(host: vscode.Uri): boolean {
+		return host && host.authority.toLowerCase() === 'github.com';
+	}
+}
+
+const exchangeCodeForToken: (host: string, state: string) => PromiseAdapter<vscode.Uri, IHostConfiguration> =
+	(host, state) => async (uri, resolve, reject) => {
+		const query = parseQuery(uri);
+		const code = query.code;
+
+		if (query.state !== state) {
+			vscode.window.showInformationMessage('Sign in failed: Received bad state');
+			reject('Received bad state');
+			return;
+		}
+
+		const post = https.request({
+			host: AUTH_RELAY_SERVER,
+			path: `/token?code=${code}&state=${query.state}`,
+			method: 'POST',
+			headers: {
+				Accept: 'application/json'
+			}
+		}, result => {
+			const buffer: Buffer[] = [];
+			result.on('data', (chunk: Buffer) => {
+				buffer.push(chunk);
+			});
+			result.on('end', () => {
+				if (result.statusCode === 200) {
+					const json = JSON.parse(Buffer.concat(buffer).toString());
+					resolve({ host, token: json.access_token });
+				} else {
+					vscode.window.showInformationMessage(`Sign in failed: ${result.statusMessage}`);
+					reject(new Error(result.statusMessage));
+				}
+			});
+		});
+
+		post.end();
+		post.on('error', err => {
+			reject(err);
+		});
+	};
+
+function parseQuery(uri: vscode.Uri) {
+	return uri.query.split('&').reduce((prev: any, current) => {
+		const queryString = current.split('=');
+		prev[queryString[0]] = queryString[1];
+		return prev;
+	}, {});
+}
+
+const manuallyEnteredToken: (host: string) => PromiseAdapter<IHostConfiguration, IHostConfiguration> =
+	host => (config: IHostConfiguration, resolve) =>
+		config.host === toCanonical(host) && resolve(config);
+
+export class GitHubServer {
+	public hostConfiguration: IHostConfiguration;
+	private hostUri: vscode.Uri;
+
+	public constructor(host: string) {
+		host = host.toLocaleLowerCase();
+		this.hostConfiguration = { host, token: undefined };
+		this.hostUri = vscode.Uri.parse(host);
+	}
+
+	public async login(): Promise<IHostConfiguration> {
+		const state = uuid();
+		const callbackUri = await vscode.env.createAppUri({ payload: { path: '/did-authenticate' } });
+		const host = this.hostUri.toString();
+		const uri = vscode.Uri.parse(`https://${AUTH_RELAY_SERVER}/authorize/?callbackUri=${encodeURIComponent(callbackUri.toString())}&scope=${SCOPES}&state=${state}&responseType=code&authServer=${host}`);
+
+		vscode.env.openExternal(uri);
+		return Promise.race([
+			promiseFromEvent(uriHandler.event, exchangeCodeForToken(host, state)),
+			promiseFromEvent(onKeychainDidChange, manuallyEnteredToken(host))
+		]);
+	}
+
+	public async validate(token?: string): Promise<IHostConfiguration> {
+		if (!token) {
+			token = this.hostConfiguration.token;
+		}
+
+		const options = GitHubManager.getOptions(this.hostUri, 'GET', '/user', token);
+
+		return new Promise<IHostConfiguration>((resolve, _) => {
+			const get = https.request(options, res => {
+				try {
+					if (res.statusCode === 200) {
+						const scopes = res.headers['x-oauth-scopes'] as string;
+						GitHubManager.validateScopes(this.hostUri, scopes);
+						resolve(this.hostConfiguration);
+					} else {
+						resolve(undefined);
+					}
+				} catch (e) {
+					Logger.appendLine(`validate() error ${e}`);
+					resolve(undefined);
+				}
+			});
+
+			get.end();
+			get.on('error', err => {
+				resolve(undefined);
+			});
+		});
+	}
+}
---
+++ vscode-pull-request-github/src/extension.ts
@@ -0,0 +1,131 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+
+import * as vscode from 'vscode';
+import TelemetryReporter from 'vscode-extension-telemetry';
+import { Repository } from './api/api';
+import { ApiImpl } from './api/api1';
+import * as Keychain from './authentication/keychain';
+import { migrateConfiguration } from './authentication/vsConfiguration';
+import { registerCommands } from './commands';
+import Logger from './common/logger';
+import { Resource } from './common/resources';
+import { handler as uriHandler } from './common/uri';
+import { formatError, onceEvent } from './common/utils';
+import * as PersistentState from './common/persistentState';
+import { EXTENSION_ID } from './constants';
+import { PullRequestManager } from './github/pullRequestManager';
+import { registerBuiltinGitProvider, registerLiveShareGitProvider } from './gitProviders/api';
+import { FileTypeDecorationProvider } from './view/fileTypeDecorationProvider';
+import { PullRequestsTreeDataProvider } from './view/prsTreeDataProvider';
+import { ReviewManager } from './view/reviewManager';
+
+const aiKey: string = 'AIF-d9b70cd4-b9f9-4d70-929b-a071c400b217';
+
+// fetch.promise polyfill
+const fetch = require('node-fetch');
+const PolyfillPromise = require('es6-promise').Promise;
+fetch.Promise = PolyfillPromise;
+
+let telemetry: TelemetryReporter;
+
+async function init(context: vscode.ExtensionContext, git: ApiImpl, repository: Repository, tree: PullRequestsTreeDataProvider): Promise<void> {
+	context.subscriptions.push(Logger);
+	Logger.appendLine('Git repository found, initializing review manager and pr tree view.');
+
+	Keychain.init(context);
+	PersistentState.init(context);
+
+	await migrateConfiguration();
+	context.subscriptions.push(Keychain.onDidChange(async _ => {
+		if (prManager) {
+			try {
+				await prManager.clearCredentialCache();
+				if (reviewManager) {
+					reviewManager.updateState();
+				}
+			} catch (e) {
+				vscode.window.showErrorMessage(formatError(e));
+			}
+		}
+	}));
+
+	context.subscriptions.push(vscode.window.registerUriHandler(uriHandler));
+	context.subscriptions.push(new FileTypeDecorationProvider());
+
+	const prManager = new PullRequestManager(repository, telemetry, git);
+	context.subscriptions.push(prManager);
+
+	const reviewManager = new ReviewManager(context, repository, prManager, tree, telemetry);
+	tree.initialize(prManager);
+	registerCommands(context, prManager, reviewManager, telemetry);
+
+	git.onDidChangeState(() => {
+		reviewManager.updateState();
+	});
+
+	git.repositories.forEach(repo => {
+		repo.ui.onDidChange(() => {
+			// No multi-select support, always show last selected repo
+			if (repo.ui.selected) {
+				prManager.repository = repo;
+				reviewManager.repository = repo;
+				tree.updateQueries();
+			}
+		});
+	});
+
+	git.onDidOpenRepository(repo => {
+		repo.ui.onDidChange(() => {
+			if (repo.ui.selected) {
+				prManager.repository = repo;
+				reviewManager.repository = repo;
+				tree.updateQueries();
+			}
+		});
+	});
+
+	/* __GDPR__
+		"startup" : {}
+	*/
+	telemetry.sendTelemetryEvent('startup');
+}
+
+export async function activate(context: vscode.ExtensionContext): Promise<ApiImpl> {
+	// initialize resources
+	Resource.initialize(context);
+	const apiImpl = new ApiImpl();
+
+	const version = vscode.extensions.getExtension(EXTENSION_ID)!.packageJSON.version;
+	telemetry = new TelemetryReporter(EXTENSION_ID, version, aiKey);
+	context.subscriptions.push(telemetry);
+
+	context.subscriptions.push(registerBuiltinGitProvider(apiImpl));
+	context.subscriptions.push(registerLiveShareGitProvider(apiImpl));
+	context.subscriptions.push(apiImpl);
+
+	Logger.appendLine('Looking for git repository');
+
+	const prTree = new PullRequestsTreeDataProvider(telemetry);
+	context.subscriptions.push(prTree);
+
+	// The Git extension API sometimes returns a single repository that does not have selected set,
+	// so fall back to the first repository if no selected repository is found.
+	const selectedRepository = apiImpl.repositories.find(repository => repository.ui.selected) || apiImpl.repositories[0];
+	if (selectedRepository) {
+		await init(context, apiImpl, selectedRepository, prTree);
+	} else {
+		onceEvent(apiImpl.onDidOpenRepository)(r => init(context, apiImpl, r, prTree));
+	}
+
+	return apiImpl;
+}
+
+export async function deactivate() {
+	if (telemetry) {
+		telemetry.dispose();
+	}
+}
